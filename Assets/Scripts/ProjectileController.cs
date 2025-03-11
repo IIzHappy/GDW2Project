34332e1b68a33418a36c48dc2d46542449f7d247
PotatoSpyware <seansteven7@icloud.com>
2025-03-11 08:48:27 -0400

    Final changes before pres

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -7,6 +7,8 @@ public class ProjectileController : MonoBehaviour
     [SerializeField] private float lifeSpan;
     [SerializeField] private bool expire = false;
     [SerializeField] private bool destroyOnCollision = true;
+    [SerializeField] private bool explode = false;
+    [SerializeField] private GameObject explosion;
 
     [SerializeField] private Rigidbody2D rb;
 
@@ -34,6 +36,15 @@ public class ProjectileController : MonoBehaviour
         damage += dmg;
     }
 
+    //private IEnumerator passThrough()
+    //{
+    //    gameObject.GetComponent<Collider2D>().isTrigger = true;
+
+    //    yield return new WaitForSeconds(0.5f);
+
+    //    gameObject.GetComponent<Collider2D>().isTrigger = true;
+    //}
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -44,9 +55,19 @@ public class ProjectileController : MonoBehaviour
         {
             collision.gameObject.GetComponent<PlayerController>().TakeDamage(damage);
         }
+
+        if (explode)
+        {
+            Instantiate(explosion, this.gameObject.transform.position, new Quaternion(0, 0, 0, 0));
+        }
         if (destroyOnCollision)
         {
             Destroy(gameObject);
         }
+        //else
+        //{
+        //    StartCoroutine(passThrough());
+        //    rb.AddForce(direction * speed);
+        //}
     }
 }
