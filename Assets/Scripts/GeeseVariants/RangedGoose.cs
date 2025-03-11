9ec9b8ce59a449266c2b5a3b34dca53535ae3af4
KaiIC5343 <jacob.hyland@ontariotechu.net>
2025-03-11 04:18:31 -0400

    bullet fix

--- Assets/Scripts/GeeseVariants/RangedGoose.cs
+++ Assets/Scripts/GeeseVariants/RangedGoose.cs
@@ -6,7 +6,7 @@ public class RangedGoose : EnemyController
 
     protected override void Attack()
     {
-        float angle = Mathf.Atan2(toPlayer.x, toPlayer.y) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(toPlayer.normalized.y, toPlayer.normalized.x) * Mathf.Rad2Deg;
         GameObject temp = Instantiate(bulletPrefab, gameObject.transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
         temp.GetComponent<ProjectileController>().direction = toPlayer.normalized;
     }
