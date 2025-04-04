36f512ed9fb153551101ace836e907930da14435
EmilyD513 <emily.dutton@ontariotechu.net>
2025-04-03 22:55:15 -0400

    Button Audio Attempt

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,8 @@ using UnityEditor;
 #endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuManager : MonoBehaviour
 {
@@ -10,20 +12,29 @@ public class MenuManager : MonoBehaviour
     [SerializeField] public string startGame;
     [SerializeField] public string credits;
 
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+
     public void StartGame()
     {
+        StartCoroutine(Wait());
         SceneManager.LoadScene(startGame);
     }
     public void MainMenu()
     {
+        StartCoroutine(Wait());
         SceneManager.LoadScene(mainMenu);
     }
     public void Credits()
     {
+        StartCoroutine(Wait());
         SceneManager.LoadScene(credits);
     }
     public void QuitGame()
     {
+        StartCoroutine(Wait());
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
