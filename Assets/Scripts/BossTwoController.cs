2a38a0c43a8f98217433de9c595c4cd724ce7c77
KaiIC5343 <jacob.hyland@ontariotechu.net>
2025-04-03 21:00:58 -0400

    adding enemy sound untested

--- Assets/Scripts/BossTwoController.cs
+++ Assets/Scripts/BossTwoController.cs
@@ -4,12 +4,14 @@ public class BossTwoController : EnemyController
 {
     public GameObject bulletPrefab;
     public GameObject enemyM;
+    public AudioClip boss2Attack;
 
     protected override void Attack()
     {
         float angle = Mathf.Atan2(toPlayer.normalized.y, toPlayer.normalized.x) * Mathf.Rad2Deg;
         GameObject temp = Instantiate(bulletPrefab, gameObject.transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
         temp.GetComponent<PushController>().direction = toPlayer.normalized;
+        AudioSource.PlayClipAtPoint(boss2Attack, gameObject.transform.position);
     }
 
     private void OnDestroy()
--- Assets/Scripts/GeeseVariants/RangedGoose.cs
+++ Assets/Scripts/GeeseVariants/RangedGoose.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class RangedGoose : EnemyController
 {
     public GameObject bulletPrefab;
+    public AudioClip enemyBulletSound;
 
     protected override void Attack()
     {
         float angle = Mathf.Atan2(toPlayer.normalized.y, toPlayer.normalized.x) * Mathf.Rad2Deg;
         GameObject temp = Instantiate(bulletPrefab, gameObject.transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
         temp.GetComponent<ProjectileController>().direction = toPlayer.normalized;
+        AudioSource.PlayClipAtPoint(enemyBulletSound, gameObject.transform.position);
     }
 }
